834a9822733b764cf2be8007d45e222e41d84e4f
kaldren <drenski666@gmail.com>
2019-03-27 21:07:19 +0200

    MVC Client configuration with IdentityServer

--- AskGoo.Auth/Config.cs
+++ AskGoo.Auth/Config.cs
@@ -40,30 +40,18 @@ namespace AskGoo.Auth
         {
             return new[]
             {
-                // client credentials flow client
-                new Client
-                {
-                    ClientId = "client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "api1" }
-                },
-
-                // MVC client using hybrid flow
+                // MVC client
                 new Client
                 {
                     ClientId = "mvc",
                     ClientName = "MVC Client",
 
-                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    ClientSecrets = { new Secret("7A0C81F5-617A-D0B2-1BF9-46FEBAFC613D".Sha256()) },
 
-                    RedirectUris = { "http://localhost:5001/signin-oidc" },
-                    FrontChannelLogoutUri = "http://localhost:5001/signout-oidc",
-                    PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:5003/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5003/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5003/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "api1" },
