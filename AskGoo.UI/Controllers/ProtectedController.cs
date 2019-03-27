542992e10b663bc0199740cf79a6c08d08234786
kaldren <drenski666@gmail.com>
2019-03-27 23:08:05 +0200

    blah

---
+++ AskGoo.UI/Controllers/ProtectedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskGoo.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProtectedController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
