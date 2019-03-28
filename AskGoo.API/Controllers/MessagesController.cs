19af4d2b78fda026795a1d6949f0f9fee06af128
kaldren <drenski666@gmail.com>
2019-03-28 20:52:56 +0200

    Messages Controller

---
+++ AskGoo.API/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskGoo.API.Controllers
+{
+    /// <summary>
+    /// Messages API
+    /// </summary>
+    public class MessagesController : ControllerBase
+    {
+        /// <summary>
+        /// Messages Constructor
+        /// </summary>
+        public MessagesController()
+        {
+        }
+    }
+}
