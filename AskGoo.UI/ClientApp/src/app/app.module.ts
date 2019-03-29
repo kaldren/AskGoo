73335b491f4fecd5d8ac55822577286d273e0d6e
kaldren <drenski666@gmail.com>
2019-03-30 01:00:56 +0200

    sidebar, footer, header, main

--- AskGoo.UI/ClientApp/src/app/app.module.ts
+++ AskGoo.UI/ClientApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
